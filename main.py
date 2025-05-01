981ac4826b7b5201a520e9d4afb15fca8e130254
Crawford L <110504159+DefNotCrawf@users.noreply.github.com>
2025-05-01 20:23:40 +1000

    Update configuration and PyQt6 application structure for platform migration (#1)
    * update files so I can move platforms
    
    * change comment

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-import pyqt6
-import pyinstaller
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton
+
+# Only needed for access to command line arguments
+import sys
+
+app = QApplication(sys.argv)
+
+window = QPushButton("Push Me")
+window.show()  # The window is not visible by default; call show() to display it
+
+app.exec()
