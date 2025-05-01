cd84818a94638f037496d956ce965fa388c2ddef
Crawford L <110504159+DefNotCrawf@users.noreply.github.com>
2025-05-01 10:18:34 +0000

    update files so I can move platforms

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-import pyqt6
-import pyinstaller
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton
+
+# Only needed for access to command line arguments
+import sys
+
+app = QApplication(sys.argv)
+
+window = QPushButton("Push Me")
+window.show()  # by default the windows doesn't show
+
+app.exec()
